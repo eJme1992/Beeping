9b2918c0d4ce3cb429c00a6016f3f7c521e4ef78
ejme1992 <edwin.jme@hotmail.com>
2024-03-24 10:04:53 -0300

    Agrega configuración de Jetstream y Sanctum***
    ***Actualiza estilos de la aplicación***
    
    ***Agrega migraciones y modelos para Jetstream***
    
    ***Actualiza el archivo de configuración de Vite***
    
    ***Actualiza el archivo composer.json***
    
    ***Actualiza el archivo package.json***
    
    ***Actualiza las rutas de autenticación***
    
    ***Actualiza el archivo de proveedores de Bootstrap***
    
    ***Actualiza el archivo de configuración de Tailwind CSS***
    
    ***Actualiza la migración de la tabla de usuarios***
    
    ***Actualiza el modelo de usuario***
    
    ***Actualiza el factory de usuarios

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ use App\Http\Controllers\ExecutedController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// ROUTES FOR AUTHENTICATION
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(
+    function(){
+   
+    Route::get('/dashboard', function () {return view('dashboard');})->name('dashboard');
+
+});
